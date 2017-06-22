b6b8dbc97b417aa57970e6f5a02fccb0f7dbda63
Josh Parsons <joshp1@joshlucy.info>
2017-06-22 09:56:09 -0700

    recipe book I was programming

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php	$fo=opendir ("./book/");
+	while ($of = readdir ($fo))
+	{	if ($of != "."&&$of !="..")
+		{	echo $of."<br />";
+			$sml = simplexml_load_file ('book/'.$of);
+			echo "name of recipe:";
+			echo "<br />";
+			echo $sml->title."<br /><br />";
+			echo "equipment needed: ";echo "<br />";
+			$lms = $sml->equipment;
+			foreach ($lms as $mxl)
+			{echo $mxl . "<br/>";}echo "<br />";
+			echo "ingredents needed:"; echo "<br />";
+			echo $sml->ingredients->caption."<br />";
+			$msl = $sml->ingredients->items->item;
+			foreach ($msl as $xml)
+			{echo $xml . "<br />";}echo "<br />";
+			echo "cooking procedure:<br/>";
+			echo $sml->procedures."<br />";
+	}	}
+	closedir ($fo);?>
